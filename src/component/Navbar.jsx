c66e00183aa2ba11116b72cbf7c0e5defe5591d1
Tayyyyylor <bryan.h.pro@gmail.com>
2023-01-01 11:10:24 +0100

    menu burger and logo done

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,20 +1,61 @@
 import React, { useContext } from "react";
+// import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Navbar.css";
 import { CartContext } from "../context/CartContextProvider";
 
 function Navbar() {
 	const { cart } = useContext(CartContext);
+	const [showLinks, setShowLinks] = useState(false);
+	const handleShowLinks = () => {
+		setShowLinks(!showLinks);
+	};
+
+	// return (
+	// 	<div>
+	// 		<header>
+	// 			<h1 class="title">My Store</h1>
+	// 			<div class="icon">
+	// 				<p class="pricenav">{cart.length}</p>
+	// 				<div>
+	// 					<i class="fa-solid fa-cart-shopping" />
+	// 				</div>
+	// 			</div>
+	// 		</header>
+	// 	</div>
+	// );
 	return (
-		<div>
-			<header>
-				<h1 class="title">My Store</h1>
-				<div class="icon">
-					<p class="pricenav">{cart.length}</p>
-					<div>
-						<i class="fa-solid fa-cart-shopping" />
-					</div>
-				</div>
-			</header>
-		</div>
+		<nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+			<div className="logo-menu">
+				<button className="navbar__burger" onClick={handleShowLinks}>
+					<span className="burger-bar" />
+				</button>
+				<h1 class="title">
+					<Link to={"/"}>My Store</Link>
+				</h1>
+			</div>
+			<div class="icon">
+				<p class="pricenav">{cart.length}</p>{" "}
+				<div>
+					<i class="fa-solid fa-cart-shopping" />{" "}
+				</div>{" "}
+			</div>
+			<div className="navbar__links">
+				<Link to={"/electronics"} className="navbar__link">
+					Electronics
+				</Link>
+				<Link to="/jewerly" className="navbar__link">
+					Jewerly
+				</Link>
+				<Link to="/menclothing" className="navbar__link">
+					Men's clothing
+				</Link>
+				<Link to="/womenclothing" className="navbar__link">
+					Women's clothing
+				</Link>
+			</div>
+		</nav>
 	);
 }
 
