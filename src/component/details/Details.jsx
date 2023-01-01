27de72a0aef2e5c94b977806ec42747fbec4195a
Tayyyyylor <bryan.h.pro@gmail.com>
2023-01-01 10:30:15 +0100

    component details données ajoutées

--- src/component/details/Details.jsx
+++ src/component/details/Details.jsx
@@ -1,6 +1,7 @@
 import { valueToNode } from "@babel/types";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./Details.css";
 
 function Details() {
@@ -15,21 +16,18 @@ function Details() {
 
 	return (
 		<main>
-			<img src="img/chart.jpg" alt="chat" />
+			<img src={product.image} alt="chat" />
 			<article id="prix">
 				<h1>{product.title}</h1>
-				<div id="montant">100eu</div>
+				<div id="montant">{product.price}</div>
 			</article>
 			<article id="description">
 				<p>Brand</p>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-					optio, culpa ratione illum officia aperiam tempore
-				</p>
+				<p>{product.description}</p>
 			</article>
 			<article id="bloc">
 				<div id="category">
-					<p>Category</p>
+					<p>{product.category}</p>
 					<div>
 						<span>
 							<i class="fa-regular fa-star" />
