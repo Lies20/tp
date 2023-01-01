4ee85ef8463ee335c111edcd5aeb3f4007fce130
Tayyyyylor <bryan.h.pro@gmail.com>
2023-01-01 09:10:52 +0100

    components details

---
+++ src/component/details/Details.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Details.css";
+
+function Details() {
+	return (
+		<main>
+			<img src="img/chart.jpg" alt="chat" />
+			<article id="prix">
+				<h1>Product</h1>
+				<div id="montant">100euros</div>
+			</article>
+			<article id="description">
+				<p>Brand</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+					optio, culpa ratione illum officia aperiam tempore
+				</p>
+			</article>
+			<article id="bloc">
+				<div id="category">
+					<p>Category</p>
+					<div>
+						<span>
+							<i class="fa-regular fa-star" />
+						</span>
+						<span>
+							<i class="fa-regular fa-star" />
+						</span>
+						<span>
+							<i class="fa-regular fa-star" />
+						</span>
+						<span>
+							<i class="fa-regular fa-star" />
+						</span>
+						<span>
+							<i class="fa-regular fa-star" />
+						</span>
+					</div>
+				</div>
+				<div id="button">Add to cart</div>
+			</article>
+		</main>
+	);
+}
+
+export default Details;
