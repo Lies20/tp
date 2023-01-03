5486489669bec13b0932eab126ff2117eabfe3cf
lies20 <hocini.lies@gmail.com>
2023-01-03 16:33:15 +0100

    list done

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,71 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function List() {
   const  [product, setProduct]=useState([])
 
-   fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+  useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+                 .then(res=>res.json())
+            .then(json => setProduct(json))
+      console.log(product)
+    }, [])
+    
+    
 
     return (
     <div>
-      <section class="list">
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 1 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 2 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 3 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 4 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 5 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 6 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
-                <article>
-                    <img class="img" src="https://previews.123rf.com/images/sudowoodo/sudowoodo1607/sudowoodo160700031/59697785-petite-ic%C3%B4ne-de-chiot-illustration-vectorielle-simple-mignon-dessin-anim%C3%A9-chien-v%C3%A9t%C3%A9rinaire-ou-anima.jpg"/>
-                  <div className='brands'>
-                      <h2 class="brand">Product 7 </h2>
-                      <p>Brand</p>
-                  </div>  
-                    <p class="price">120£</p>
-                </article>
+            <section class="list">
+                {product.slice(0,7)
+                    .map((value, index) => (
+                    <article>
+                        <img class="img" src={value.image} />
+                        <div className='brands'>
+                            <h2 class="brand"> </h2>
+                                <p>{value.title}</p>
+                        </div>
+                            <p class="price">{value.price}</p>
+                    </article>
+                ))}
     </section>
     </div>
   )
